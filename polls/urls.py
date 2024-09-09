43dbfec42dfabd19774f48a2544fb2656beb52f8
1NJ57C3 <85198277+1NJ57C3@users.noreply.github.com>
2024-09-09 04:45:25 -0700

    Part 3 Tutorial complete - Add Views and Namespacing

--- polls/urls.py
+++ polls/urls.py
@@ -2,7 +2,23 @@ from django.urls import path
 
 from . import views
 
-# Set route for `./views.py`
+"""
+Django uses `urlpatterns` variable to interpret URLs
+Params from URLs are interpreted using angle brackets
+  This object contains a data type converter, colon separator, and param name.
+    Path converters : https://docs.djangoproject.com/en/5.1/topics/http/urls/#path-converters
+"""
+
+# Add "polls" namespace
+app_name = "polls"
+# Set routes for `./views.py`
 urlpatterns = [
+  # Route: /polls/
   path("", views.index, name="index"),
+  # Route: /polls/:id/
+  path("<int:question_id>/", views.detail, name="detail"),
+  # Route: /polls/:id/results
+  path("<int:question_id>/results/", views.results, name="results"),
+  # Route: /polls/:id/vote/
+  path("<int:question_id>/vote/", views.vote, name="vote"),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,51 @@
-from django.shortcuts import render
+# from django.http import Http404 # No longer needed with `get_object_or_404` shortcut
+from django.shortcuts import get_object_or_404, render
 
+"""
+The following imports are no longer if using the `render()` shortcut (imported above) to render templates for all `views`
+"""
 from django.http import HttpResponse
+# from django.template import loader
+
+from .models import Question
 
 # Display this at `localhost:8000/polls/`
 def index(request):
-  return HttpResponse("Goodbye, world. You're at the polls index.")
+  latest_question_list = Question.objects.order_by("-pub_date")[:5]
+  
+  # * Display latest 5 poll questions separated by comma
+  # output = ", ".join([q.question_text for q in latest_question_list])
+  # return HttpResponse(output)
+
+  # * Load `index` template instead of previous hard-coded response
+  # template = loader.get_template("polls/index.html")
+  # context = {
+  #   "latest_question_list": latest_question_list,
+  # }
+  # return HttpResponse(template.render(context, request))
+  
+  # * Using `render()` shortcut:
+  context = {"latest_question_list": latest_question_list}
+  return render(request, "polls/index.html", context)
+
+def detail(request, question_id):
+  # * Basic stub response
+  # return HttpResponse("You're looking at question %s." % question_id)
+
+  # * Attempt to load `detail` template or raise 404
+  # try:
+  #   question = Question.objects.get(pk=question_id)
+  # except Question.DoesNotExist:
+  #   raise Http404("Question does not exist")
+  # return render(request, "polls/detail.html", {"question": question})
+
+  # * Using `get_object_or_404()` shortcut
+  question = get_object_or_404(Question, pk=question_id)
+  return render(request, "polls/detail.html", {"question": question})
+
+def results (request, question_id):
+  response = "You're looking at the results of question %s."
+  return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+  return HttpResponse("You're voting on question %s." % question_id)
