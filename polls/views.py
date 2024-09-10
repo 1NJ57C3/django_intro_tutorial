4052b2e0a320ce2820111e7079d4421976ca86fa
1NJ57C3 <85198277+1NJ57C3@users.noreply.github.com>
2024-09-10 04:27:58 -0700

    Part 5 Tutorial checkpoint - Add IndexView tests, update  function in /

--- polls/views.py
+++ polls/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 
 from .models import Choice, Question
 
@@ -22,21 +23,28 @@ from .models import Choice, Question
 #   question = get_object_or_404(Question, pk=question_id)
 #   return render(request, "polls/results.html", {"question": question})
 
+
 class IndexView(generic.ListView):
   template_name = "polls/index.html"
   context_object_name = "latest_question_list"
 
   def get_queryset(self):
-    """Return the last five published questions."""
-    return Question.objects.order_by("-pub_date")[:5]
-  
+    """
+    Return the last five published questions (not including those set to be
+    published in the future).
+    """
+    return Question.objects.filter(pub_date__lte=timezone.now()).order_by("-pub_date")[:5]
+    # Above line returns a queryset containing `Question`s whose `pub_date` is less than or equal (__lte) to `timezone.now`.
+
 class DetailView(generic.DetailView):
   model = Question
   template_name = "polls/detail.html"
 
+
 class ResultsView(generic.DetailView):
   model = Question
   template_name = "polls/results.html"
+
 
 def vote(request, question_id):
   question = get_object_or_404(Question, pk=question_id)
