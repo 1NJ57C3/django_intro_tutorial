315749be8c7bf2bd2bca8daba760ecb2101c6924
1NJ57C3 <85198277+1NJ57C3@users.noreply.github.com>
2024-09-12 16:49:49 -0700

    Modify comment at line 37 - improve clarity

--- polls/views.py
+++ polls/views.py
@@ -34,7 +34,7 @@ class IndexView(generic.ListView):
     published in the future).
     """
     return Question.objects.filter(pub_date__lte=timezone.now()).order_by("-pub_date")[:5]
-    # Above line returns a queryset containing `Question`s whose `pub_date` is less than or equal (__lte) to `timezone.now`.
+    # Returns filtered `Question`s whose `pub_date` is less than or equal (__lte) to `timezone.now`
 
 class DetailView(generic.DetailView):
   model = Question
